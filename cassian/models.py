2fa5484c7279ef0952b43f6b14a2cfc675304901
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-06-18 17:47:56 -0500

    Simplifies CassianModel output layers by replacing each Dense+TimeDistributed output with a SimpleRNN. Also declares loss functions.

--- cassian/models.py
+++ cassian/models.py
@@ -2,11 +2,10 @@
 @author: Luis I. Reyes Castro
 """
 
-import copy as cp
 import numpy as np
 from keras import backend as K
 from keras.layers import Input, Dense
-from keras.layers.wrappers import TimeDistributed
+from keras.layers.recurrent import SimpleRNN
 from keras.models import Model
 from keras.utils.vis_utils import plot_model
 from .layers import VectorDependentGatedRNN
@@ -15,17 +14,29 @@ from .layers import VectorDependentGatedRNN
 class CassianModel :
 
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    def __init__( self, batch_specs, vector_embedding_dim = 16,
-                                     layer_sizes = [ 256, 256, 256] ) :
+    def __init__( self, dataset, vector_embedding_dim = 16,
+                                 layer_sizes = [ 256, 256, 256] ) :
 
-        self.specs = cp.deepcopy(batch_specs)
-#        self.batch_size       = self.specs.batch_size
-#        self.vec_dim          = self.specs.vec_dim
-#        self.ts_dim           = self.specs.ts_dim
+        def exponential( tensor) :
+            return K.exp(tensor)
+
+        def zero_one_softsign( tensor) :
+            return 0.5 + 0.5 * K.softsign( 2.0 * tensor )
+
+        self.dataset          = dataset
+        self.specs            = dataset.batch_specs
+
+        if self.specs is None :
+            raise ValueError( 'Dataset sampler method has not been setup' )
+
         self.embed_dim        = vector_embedding_dim
         self.layer_sizes      = layer_sizes
         self.learnable_layers = []
         self.outputs_list     = []
+        self.loss_functions   = {}
+
+        # -----------------------------------------------------------------------------
+        # Builds the input layers and the dimensionality reduction layer
 
         self.X_vecs_shape = ( self.specs.batch_size, self.specs.vec_dim)
         self.X_ts_shape   = ( self.specs.batch_size, None, self.specs.ts_dim)
@@ -39,6 +50,9 @@ class CassianModel :
                                 activation = 'softsign',
                                 name = layer_name )( self.X_vecs)
         self.learnable_layers.append( layer_name )
+
+        # -----------------------------------------------------------------------------
+        # Builds a stack of several layers of Vector-dependent gated RNNs
 
         self.layer_outputs = [ self.X_ts ]
 
@@ -63,57 +77,58 @@ class CassianModel :
 
         self.layer_outputs = self.layer_outputs[1:]
 
-        exp_activation      = lambda tensor : K.exp(tensor)
-        zero_one_activation = lambda tensor : 0.5 + 0.5 * K.softsign( 2.0 * tensor )
+        # -----------------------------------------------------------------------------
+        # Builds the first two output timeseries: sold and is-on-sale
+        # The first output is the mean of a Poisson random variable
+        # (a strictly positive real number) and is trained with Poisson loss
+        # The second output is binary and is trained with binary cross-entropy
 
-        layer_name = 'Output-1'
-        layer = Dense( units = 1,
-                       input_dim = self.layer_sizes[-1],
-                       activation = exp_activation,
-                       name = layer_name )
-        TD_name = 'Sold_TS'
-        Y_hat_sales = \
-        TimeDistributed( layer, name = TD_name )( self.layer_outputs[-1] )
+        layer_names = [ 'Sold', 'Is_On_Sale' ]
+        layer_activations = [ exponential, zero_one_softsign ]
+        layer_losses = [ 'poisson', 'binary_crossentropy' ]
 
-        self.learnable_layers.append( TD_name )
-        self.outputs_list.append( Y_hat_sales )
+        for ( layer_name, layer_activation, layer_loss) in \
+            zip( layer_names, layer_activations, layer_losses) :
 
-        layer_name = 'Output-2'
-        layer = Dense( units = 1,
-                       input_dim = self.layer_sizes[-1],
-                       activation = zero_one_activation,
-                       name = layer_name )
-        TD_name = 'Is_on_sale_TS'
-        Y_hat_is_on_sale = \
-        TimeDistributed( layer, name = TD_name )( self.layer_outputs[-1] )
+            layer_object = SimpleRNN( name = layer_name,
+                                      units = 1,
+                                      activation = layer_activation,
+                                      return_sequences = True)
+            output_tensor = layer_object( self.layer_outputs[-1] )
 
-        self.learnable_layers.append( TD_name )
-        self.outputs_list.append( Y_hat_is_on_sale )
+            self.learnable_layers.append( layer_name)
+            self.outputs_list.append( output_tensor)
+            self.loss_functions[layer_name] = layer_loss
 
-        cat_out_index = tuple( range(5) )
-        cat_out_names = [ 'Replenished', 'Returned', 'Trashed',
-                          'Found', 'Missing' ]
-        cat_out_dims  = [ self.specs.ts_replenished_dim,
-                          self.specs.ts_returned_dim,
-                          self.specs.ts_trashed_dim,
-                          self.specs.ts_found_dim,
-                          self.specs.ts_missing_dim ]
+        # -----------------------------------------------------------------------------
+        # Builds the last five output timeseries, each of which is a
+        # probability softmax (i.e. a softmax) trained with
+        # sparse categorical cross-entropy
 
-        for ( i, name, dim) in zip( cat_out_index, cat_out_names, cat_out_dims) :
-            if dim is not None :
-                layer_name = 'Output-' + str( 3 + i )
-                layer = Dense( units = dim,
-                               input_dim = self.layer_sizes[-1],
-                               activation = 'softmax',
-                               name = layer_name )
-                TD_name = name + '_TS'
-                Z_hat = TimeDistributed( layer,
-                                         name = TD_name)( self.layer_outputs[-1] )
-                self.learnable_layers.append( TD_name )
-                self.outputs_list.append( Z_hat )
+        layer_names = [ 'Replenished', 'Returned', 'Trashed', 'Found', 'Missing' ]
+        layer_dims  = [ self.specs.ts_replenished_dim,
+                        self.specs.ts_returned_dim,
+                        self.specs.ts_trashed_dim,
+                        self.specs.ts_found_dim,
+                        self.specs.ts_missing_dim ]
+        layer_losses = 'sparse_categorical_crossentropy'
+
+        for ( layer_name, layer_dim) in zip( layer_names, layer_dims) :
+
+            layer_object = SimpleRNN( name = layer_name,
+                                      units = layer_dim,
+                                      activation = 'softmax',
+                                      return_sequences = True)
+            output_tensor = layer_object( self.layer_outputs[-1] )
+
+            self.learnable_layers.append( layer_name)
+            self.outputs_list.append( output_tensor)
+            self.loss_functions[layer_name] = layer_losses
 
         self.model = Model( inputs = [ self.X_vecs, self.X_ts],
                             outputs = self.outputs_list )
+
+        self.model.compile( optimizer = 'adam', loss = self.loss_functions)
 
         return
 
--- driver.py
+++ driver.py
@@ -9,7 +9,8 @@ import numpy as np
 dataset = Dataset.load( store_id = 101)
 dataset.setup_sampler( batch_size = 32, timesteps = 90)
 
-cass = CassianModel( dataset.batch_specs)
+cass = CassianModel( dataset)
+cass.plot_model()
 
 dataset_dict = dataset.as_dictionary()
 batch = dataset.sample_batch()
