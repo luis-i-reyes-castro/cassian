45b4077baa4731bf82a91722ebcb7bc9346b9c32
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-06-29 15:36:44 -0500

    Removes legacy plotting function from convenience.

--- cassian/convenience.py
+++ cassian/convenience.py
@@ -4,7 +4,6 @@
 
 import datetime as dt
 from datetime import datetime as dtdt
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
@@ -72,36 +71,3 @@ def save_df_to_excel( dataframe, output_file, output_sheet = 'Sheet') :
     writer.save
 
     return
-
-# -------------------------------------------------------------------------------------
-def plot_timeseries( ts_1, ts_2) :
-
-    COLOR_TS_1 = 'blue'
-    COLOR_TS_2 = 'red'
-    LINEWIDTH  = 0.8
-    MARKERSIZE = 4
-
-    time = np.arange( len(ts_1) ) + 1
-
-    plt.plot( time, ts_1, label = 'ts_{1}', \
-              linestyle = '-', linewidth = LINEWIDTH, color = COLOR_TS_1, \
-              marker = 'o', markerfacecolor = COLOR_TS_1, \
-              markeredgecolor = 'None', markersize = MARKERSIZE)
-
-    plt.plot( time, ts_2, label = 'ts_{2}', \
-              linestyle = '-', linewidth = LINEWIDTH, color = COLOR_TS_2, \
-              marker = 'o', markerfacecolor = COLOR_TS_2, \
-              markeredgecolor = 'None', markersize = MARKERSIZE)
-
-    plt.show()
-
-#    plt.grid()
-#    plt.xlim( ( -1, len(ts_x1)) )
-#    plt.ylim( ( -1.10, +1.10) )
-#    plt.xlabel( 't', fontsize = FONTSIZE_AXIS_LABEL )
-#    plt.ylabel( 'x_1(t), x_2(t), y(t)', fontsize = FONTSIZE_AXIS_LABEL )
-#    plt.legend( fontsize = FONTSIZE_LEGEND )
-
-#    if output_file is not None :
-#        plt.savefig( output_file, format = 'eps', dpi = 300, bbox_inches ='tight')
-#    plt.show()
