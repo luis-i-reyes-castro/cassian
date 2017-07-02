99d38162fa07526e7e3849f95dd7ae674cbecebd
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-07-02 16:48:43 -0500

    Updates driver script to do results analysis.

--- driver.py
+++ driver.py
@@ -1,32 +1,19 @@
 ## -------------------------------------------------------------------------------------
-#from cassian.connectivity import DatabaseClient
-#
-#client = DatabaseClient( store_id = 101)
-#client.fetch_data( intro_year_limit = 2015,
-#                   min_num_of_records = 180, reuse_downloaded_result_sets=True)
-
-# -------------------------------------------------------------------------------------
-from cassian.models import CassianModel
-
-dataset = 'dataset-103/ready-dataset.pkl'
-cassian = CassianModel( dataset, batch_size = 16, timesteps = 90)
-
-cassian.plot_model()
-
-#cassian.train_on_dataset( epochs = 1, workers = 8)
-
-#cassian.train_on_dataset( epochs = 1, workers = 8)
-#cassian.save()
-#cassian.train_on_dataset( epochs = 100, workers = 8)
-#cassian.save()
-#cassian.compute_predictions()
-
-## -------------------------------------------------------------------------------------
 #from cassian.models import CassianModel
 #
-#model_file = 'trained-models/store-101_model.pkl'
-#cassian = CassianModel.load( model_file)
+#dataset = 'dataset-103/ready-dataset.pkl'
+#cassian = CassianModel( dataset, batch_size = 16, timesteps = 90)
 #
-##cassian.train_on_dataset( epochs = 60, workers = 8)
-##cassian.save()
-#cassian.compute_predictions()
+#cassian.save_model()
+
+# -------------------------------------------------------------------------------------
+from cassian.convenience import de_serialize
+from matplotlib import style
+
+results     = de_serialize('results/store-103_results.pkl')
+summary     = results['summary']
+predictions = results['predictions']
+
+def plot_predictions( sku) :
+    style.use('ggplot')
+    return predictions[sku].plot()
