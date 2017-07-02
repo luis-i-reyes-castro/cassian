855348f645c0a27bbf57b4b383d28e316e557dc7
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-07-02 16:12:43 -0500

    Improves user interface: Sorts import statements.

--- Cassian.py
+++ Cassian.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 import sys, getopt
-from cassian.connectivity import DatabaseClient
-from cassian.data_management import Dataset
-from cassian.models import CassianModel
 
 def show_usage() :
-    print( 'Cassian-Execute: Usage:' )
-    print( './cassian-execute.py -h|--help' )
-    print( './cassian-execute.py -l|--list' )
-    print( './cassian-execute.py -s|--store <store_id> ' +
+    print( 'CASSIAN - Usage:' )
+    print( './Cassian.py -h|--help' )
+    print( './Cassian.py -l|--list' )
+    print( './Cassian.py -s|--store <store_id> ' +
            '[-f|--fetch] [-r|--resume_fetch] [-l|--load] [-T|--train] ' +
            '[-e|--epochs] <num_of_epochs> ' +
            '[-b|--batch_size] <batch_size> ' +
@@ -49,28 +46,41 @@ def main( argv) :
             return
 
         if opt in ( '-l', '--list') :
+
+            from cassian.connectivity import DatabaseClient
+
             client = DatabaseClient()
             client.get_list_of_stores()
+
             return
 
         if opt in ( '-s', '--store') :
             store_id = int(arg)
+
         if opt in ( '-f', '--fetch') :
             mode_fetch = True
+
         if opt in ( '-r', '--resume_fetch') :
             resume = True
+
         if opt in ( '-l', '--load') :
             mode_load = True
+
         if opt in ( '-T', '--train') :
             mode_train = True
+
         if opt in ( '-e', '--epochs') :
             epochs = max( ( epochs, int(arg) ) )
+
         if opt in ( '-b', '--batch_size') :
             batch_size = max( ( batch_size, int(arg) ) )
+
         if opt in ( '-t', '--timesteps') :
             timesteps = max( ( timesteps, int(arg) ) )
+
         if opt in ( '-w', '--workers') :
             workers = max( ( 2, int(arg) ) )
+
         if opt in ( '-p', '--predict') :
             mode_predict = True
 
@@ -80,14 +90,21 @@ def main( argv) :
         sys.exit()
 
     if mode_fetch :
+
+        from cassian.connectivity import DatabaseClient
+        from cassian.data_management import Dataset
+
         client = DatabaseClient( store_id = store_id)
         client.fetch_data( intro_year_limit = 2015,
                            min_num_of_records = 180,
                            reuse_downloaded_result_sets = resume)
+
         dataset = Dataset( raw_data_file = client.output_file)
         dataset.save()
 
-    if mode_train or mode_predict :
+    if mode_load or mode_train or mode_predict :
+
+        from cassian.models import CassianModel
 
         if mode_load :
             model_file = 'trained-models/store-' + str(store_id) + '_model.pkl'
