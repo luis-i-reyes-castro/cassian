b6c0596a9969135c13e440ba368f186c1bf94c72
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-07-03 01:15:11 -0500

    Moves class TimeseriesCategorizer into class Dataset and renames several members of class SkuData so that they are easier to understand.

--- cassian/data_management.py
+++ cassian/data_management.py
@@ -43,20 +43,65 @@ class Dataset :
     categorizer = {}
     data        = {}
 
-    num_timesteps      = 0
-    vec_dim            = 0
-    ts_dim             = 0
-    ts_replenished_dim = 0
-    ts_returned_dim    = 0
-    ts_trashed_dim     = 0
-    ts_found_dim       = 0
-    ts_missing_dim     = 0
-    list_of_sku_probs  = []
+    num_timesteps     = 0
+    vec_dim           = 0
+    ts_dim            = 0
+    z_replenished_dim = 0
+    z_returned_dim    = 0
+    z_trashed_dim     = 0
+    z_found_dim       = 0
+    z_missing_dim     = 0
+    list_of_sku_probs = []
 
     vec_mean = np.array([])
     vec_std  = np.array([])
     ts_mean  = np.array([])
     ts_std   = np.array([])
+
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    class TimeseriesCategorizer :
+
+        bins           = []
+        categories     = []
+        num_categories = 0
+
+        def __init__( self, max_val = 12) :
+
+            self.bins = [ -1, 0, 1, 2, 3, 4, 5, 6, 8, 10, 12 ]
+
+            increment_steps = 6
+            increment_value = 12
+
+            while self.bins[-1] < max_val :
+
+                for step in range(increment_steps) :
+
+                    self.bins.append( self.bins[-1] + increment_value )
+
+                    if self.bins[-1] >= max_val :
+                        break
+
+                increment_value *= 2
+
+            self.categories     = self.bins[1:]
+            self.num_categories = len( self.categories )
+
+            return
+
+        def categorize( self, input_ts) :
+
+            categorical_ts = pd.cut( pd.Series( input_ts[:,0] ), bins = self.bins)
+            integer_vector = categorical_ts.cat.codes
+            integer_vector = integer_vector.as_matrix().astype('uint32')
+            return integer_vector[ :, None]
+
+        def get_category_at_index( self, index) :
+
+            return self.categories[index]
+
+        def get_copy( self) :
+
+            return cp.deepcopy(self)
 
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     def __init__( self, raw_data_file = None) :
@@ -80,36 +125,65 @@ class Dataset :
         self.vectors = pd.get_dummies( self.info_main)
 
         self.categorizer = {}
-        self.categorizer['replenished'] = \
-        TimeseriesCategorizer( max_val = self.info_other['REPLENISHED_MAX'].max() )
-        self.categorizer['returned']    = \
-        TimeseriesCategorizer( max_val = self.info_other['RETURNED_MAX'].max() )
-        self.categorizer['trashed']     = \
-        TimeseriesCategorizer( max_val = self.info_other['TRASHED_MAX'].max() )
-        self.categorizer['found']       = \
-        TimeseriesCategorizer( max_val = self.info_other['FOUND_MAX'].max() )
-        self.categorizer['missing']     = \
-        TimeseriesCategorizer( max_val = self.info_other['MISSING_MAX'].max() )
+
+        self.categorizer['Z1'] = \
+        self.TimeseriesCategorizer( self.info_other['REPLENISHED_MAX'].max() )
+
+        self.categorizer['Z2'] = \
+        self.TimeseriesCategorizer( self.info_other['RETURNED_MAX'].max() )
+
+        self.categorizer['Z3'] = \
+        self.TimeseriesCategorizer( self.info_other['TRASHED_MAX'].max() )
+
+        self.categorizer['Z4'] = \
+        self.TimeseriesCategorizer( self.info_other['FOUND_MAX'].max() )
+
+        self.categorizer['Z5'] = \
+        self.TimeseriesCategorizer( self.info_other['MISSING_MAX'].max() )
 
         self.data = {}
+
         for sku in self.list_of_skus :
+
             print( 'Building SkuData object for SKU:', str(sku))
-            vec_ = self.vectors.loc[sku]
-            ts_  = raw_data['timeseries'][ sku]
-            self.data[sku] = SkuData( vector = vec_,
-                                      timeseries = ts_,
-                                      categorizers = self.categorizer)
+            self.data[sku] = SkuData( vector = self.vectors.loc[sku],
+                                      timeseries = raw_data['timeseries'][ sku] )
+
+            self.data[sku].z_replenished = \
+            self.categorizer['Z1'].categorize( self.data[sku].y_replenished )
+            self.data[sku].z_replenished_dim = \
+            self.categorizer['Z1'].num_categories
+
+            self.data[sku].z_returned = \
+            self.categorizer['Z2'].categorize( self.data[sku].y_returned )
+            self.data[sku].z_returned_dim = \
+            self.categorizer['Z2'].num_categories
+
+            self.data[sku].z_trashed = \
+            self.categorizer['Z3'].categorize( self.data[sku].y_trashed )
+            self.data[sku].z_trashed_dim = \
+            self.categorizer['Z3'].num_categories
+
+            self.data[sku].z_found = \
+            self.categorizer['Z4'].categorize( self.data[sku].y_found )
+            self.data[sku].z_found_dim = \
+            self.categorizer['Z4'].num_categories
+
+            self.data[sku].z_missing = \
+            self.categorizer['Z5'].categorize( self.data[sku].y_missing )
+            self.data[sku].z_missing_dim = \
+            self.categorizer['Z5'].num_categories
 
         self.update_num_timesteps_and_list_of_sku_probs()
 
-        arbitrary_sku_obj       = self.data[ self.list_of_skus[0] ]
-        self.vec_dim            = arbitrary_sku_obj.vec_dim
-        self.ts_dim             = arbitrary_sku_obj.ts_dim
-        self.ts_replenished_dim = arbitrary_sku_obj.ts_replenished_dim
-        self.ts_returned_dim    = arbitrary_sku_obj.ts_returned_dim
-        self.ts_trashed_dim     = arbitrary_sku_obj.ts_trashed_dim
-        self.ts_found_dim       = arbitrary_sku_obj.ts_found_dim
-        self.ts_missing_dim     = arbitrary_sku_obj.ts_missing_dim
+        arbitrary_sku_obj      = self.data[ self.list_of_skus[0] ]
+        self.vec_dim           = arbitrary_sku_obj.vec_dim
+        self.ts_dim            = arbitrary_sku_obj.ts_dim
+        self.z_replenished_dim = arbitrary_sku_obj.z_replenished_dim
+        self.z_returned_dim    = arbitrary_sku_obj.z_returned_dim
+        self.z_trashed_dim     = arbitrary_sku_obj.z_trashed_dim
+        self.z_found_dim       = arbitrary_sku_obj.z_found_dim
+        self.z_missing_dim     = arbitrary_sku_obj.z_missing_dim
 
         self.compute_mean_std()
         self.normalize_timeseries()
@@ -233,11 +307,11 @@ class Dataset :
 
             ds_obj.vec_dim            = self.vec_dim
             ds_obj.ts_dim             = self.ts_dim
-            ds_obj.ts_replenished_dim = self.ts_replenished_dim
-            ds_obj.ts_returned_dim    = self.ts_returned_dim
-            ds_obj.ts_trashed_dim     = self.ts_trashed_dim
-            ds_obj.ts_found_dim       = self.ts_found_dim
-            ds_obj.ts_missing_dim     = self.ts_missing_dim
+            ds_obj.z_replenished_dim = self.z_replenished_dim
+            ds_obj.z_returned_dim    = self.z_returned_dim
+            ds_obj.z_trashed_dim     = self.z_trashed_dim
+            ds_obj.z_found_dim       = self.z_found_dim
+            ds_obj.z_missing_dim     = self.z_missing_dim
 
             ds_obj.compute_mean_std()
             ds_obj.normalize_timeseries()
@@ -245,53 +319,6 @@ class Dataset :
             dataset_objects.append( ds_obj)
 
         return ( dataset_objects[0], dataset_objects[1])
-
-# =====================================================================================
-class TimeseriesCategorizer :
-
-    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    bins           = []
-    categories     = []
-    num_categories = 0
-
-    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    def __init__( self, max_val = 12) :
-
-        self.bins       = [ -1, 0, 1, 2, 3, 4, 5, 6, 8, 10, 12 ]
-        increment_steps = 6
-        increment_value = 12
-
-        while self.bins[-1] < max_val :
-
-            for _ in range( increment_steps) :
-                self.bins.append( self.bins[-1] + increment_value )
-                if self.bins[-1] >= max_val :
-                    break
-
-            increment_value *= 2
-
-        self.categories     = self.bins[1:]
-        self.num_categories = len( self.categories )
-
-        return
-
-    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    def categorize( self, input_ts) :
-
-        categorical_ts = pd.cut( pd.Series( input_ts[:,0] ), bins = self.bins)
-        integer_vector = categorical_ts.cat.codes
-        integer_vector = integer_vector.as_matrix().astype('uint32')
-        return integer_vector[ :, None]
-
-    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    def get_category_at_index( self, index) :
-
-        return self.categories[index]
-
-    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    def get_copy( self) :
-
-        return cp.deepcopy(self)
 
 # =====================================================================================
 class SkuData :
@@ -306,33 +333,33 @@ class SkuData :
     ts     = np.array([])
     ts_dim = 0
 
-    ts_sold        = np.array([])
-    ts_is_on_sale  = np.array([])
-    ts_replenished = np.array([])
-    ts_returned    = np.array([])
-    ts_trashed     = np.array([])
-    ts_found       = np.array([])
-    ts_missing     = np.array([])
+    y_sold        = np.array([])
+    y_is_on_sale  = np.array([])
+    y_replenished = np.array([])
+    y_returned    = np.array([])
+    y_trashed     = np.array([])
+    y_found       = np.array([])
+    y_missing     = np.array([])
 
-    ts_replenished_cat = np.array([])
-    ts_replenished_dim = 0
-    ts_returned_cat    = np.array([])
-    ts_returned_dim    = 0
-    ts_trashed_cat     = np.array([])
-    ts_trashed_dim     = 0
-    ts_found_cat       = np.array([])
-    ts_found_dim       = 0
-    ts_missing_cat     = np.array([])
-    ts_missing_dim     = 0
+    z_replenished     = np.array([])
+    z_replenished_dim = 0
+    z_returned        = np.array([])
+    z_returned_dim    = 0
+    z_trashed         = np.array([])
+    z_trashed_dim     = 0
+    z_found           = np.array([])
+    z_found_dim       = 0
+    z_missing         = np.array([])
+    z_missing_dim     = 0
 
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    def __init__( self, vector, timeseries, categorizers) :
+    def __init__( self, vector, timeseries) :
 
-        self.vector = vector
-        self.vec = self.vector.as_matrix().astype('float32')
+        self.vector  = vector
+        self.vec     = self.vector.as_matrix().astype('float32')
         self.vec_dim = self.vec.shape[0]
 
-        self.timeseries = timeseries
+        self.timeseries    = timeseries
         self.num_timesteps = len( self.timeseries ) - 1
 
         ts_cols = self.timeseries.columns.tolist()
@@ -346,36 +373,17 @@ class SkuData :
         self.ts     = self.timeseries[ ts_cols ].as_matrix().astype('float32')
         self.ts_dim = self.ts.shape[1]
 
-        self.ts_sold       = \
+        self.y_sold = \
         self.timeseries[ ['SOLD'] ].as_matrix().astype('float32')
-        self.ts_is_on_sale = \
+
+        self.y_is_on_sale = \
         self.timeseries[ ['IS_ON_SALE'] ].as_matrix().astype('float32')
 
-        self.ts_replenished = self.timeseries[ ['REPLENISHED'] ].as_matrix()
-        self.ts_returned    = self.timeseries[ ['RETURNED'] ].as_matrix()
-        self.ts_trashed     = self.timeseries[ ['TRASHED'] ].as_matrix()
-        self.ts_found       = self.timeseries[ ['FOUND'] ].as_matrix()
-        self.ts_missing     = self.timeseries[ ['MISSING'] ].as_matrix()
-
-        self.ts_replenished_cat = \
-        categorizers['replenished'].categorize( self.ts_replenished )
-        self.ts_replenished_dim = categorizers['replenished'].num_categories
-
-        self.ts_returned_cat    = \
-        categorizers['returned'].categorize( self.ts_returned )
-        self.ts_returned_dim = categorizers['replenished'].num_categories
-
-        self.ts_trashed_cat     = \
-        categorizers['trashed'].categorize( self.ts_trashed )
-        self.ts_trashed_dim = categorizers['trashed'].num_categories
-
-        self.ts_found_cat       = \
-        categorizers['found'].categorize( self.ts_found )
-        self.ts_found_dim = categorizers['found'].num_categories
-
-        self.ts_missing_cat     = \
-        categorizers['missing'].categorize( self.ts_missing )
-        self.ts_missing_dim = categorizers['missing'].num_categories
+        self.y_replenished = self.timeseries[ ['REPLENISHED'] ].as_matrix()
+        self.y_returned    = self.timeseries[ ['RETURNED'] ].as_matrix()
+        self.y_trashed     = self.timeseries[ ['TRASHED'] ].as_matrix()
+        self.y_found       = self.timeseries[ ['FOUND'] ].as_matrix()
+        self.y_missing     = self.timeseries[ ['MISSING'] ].as_matrix()
 
         return
 
@@ -422,25 +430,25 @@ class SkuData :
         i_start += 1
         i_end   += 1
 
-        np.copyto( src = self.ts_sold[ i_start : i_end, :],
+        np.copyto( src = self.y_sold[ i_start : i_end, :],
                    dst = batch_sample.Y_sold[ sample_index, :, :] )
 
-        np.copyto( src = self.ts_is_on_sale[ i_start : i_end, :],
+        np.copyto( src = self.y_is_on_sale[ i_start : i_end, :],
                    dst = batch_sample.Y_is_on_sale[ sample_index, :, :] )
 
-        np.copyto( src = self.ts_replenished_cat[ i_start : i_end, :],
+        np.copyto( src = self.z_replenished[ i_start : i_end, :],
                    dst = batch_sample.Z_replenished[ sample_index, :, :] )
 
-        np.copyto( src = self.ts_returned_cat[ i_start : i_end, :],
+        np.copyto( src = self.z_returned[ i_start : i_end, :],
                    dst = batch_sample.Z_returned[ sample_index, :, :] )
 
-        np.copyto( src = self.ts_trashed_cat[ i_start : i_end, :],
+        np.copyto( src = self.z_trashed[ i_start : i_end, :],
                    dst = batch_sample.Z_trashed[ sample_index, :, :] )
 
-        np.copyto( src = self.ts_found_cat[ i_start : i_end, :],
+        np.copyto( src = self.z_found[ i_start : i_end, :],
                    dst = batch_sample.Z_found[ sample_index, :, :] )
 
-        np.copyto( src = self.ts_missing_cat[ i_start : i_end, :],
+        np.copyto( src = self.z_missing[ i_start : i_end, :],
                    dst = batch_sample.Z_missing[ sample_index, :, :] )
 
         return
