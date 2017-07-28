39d48e4dd4da12e7b4bc17a4d6872d514221b751
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-07-28 01:09:46 -0500

    Updates experiments script.

--- Run-experiments.sh
+++ Run-experiments.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-STORE=443
-NUM_RUNS=10
-EPOCHS_PER_RUN=10
+STORE=103
+NUM_RUNS=20
+EPOCHS_PER_RUN=40
 
 for i in $(seq 1 $NUM_RUNS)
 do
-./Cassian.py --store $STORE --train --epochs $EPOCHS_PER_RUN
+./Cassian.py --store $STORE --train --epochs $EPOCHS_PER_RUN --batch_size 64
 done
