d682c82f7d6b85201a5be59b2ef2afde50745038
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-07-31 19:28:31 -0500

    Decreases learning rate to 2E-4.

--- Run-experiments.sh
+++ Run-experiments.sh
@@ -4,14 +4,14 @@ STORE=103
 NUM_RUNS=4
 EPOCHS_PER_RUN=400
 BATCH_SIZE=128
-LEARNING_RATE=1E-3
+LEARNING_RATE=2E-4
 
 rm trained-models/*
 rm results/*
 
 for i in $(seq 1 $NUM_RUNS)
 do
-for REGULARIZATION in 4E-6 1E-5 2E-5 4E-5 1E-4
+for REGULARIZATION in 1E-5 2E-5 4E-5 1E-4
 do
 ./Cassian.py --store $STORE --train --epochs $EPOCHS_PER_RUN --batch-size $BATCH_SIZE --regularization $REGULARIZATION --learning-rate $LEARNING_RATE
 done
