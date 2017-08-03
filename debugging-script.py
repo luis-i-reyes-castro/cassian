ca55da79ac43ee166d38f5384349f9dbb69bc3cf
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-08-03 01:33:26 -0500

    Updates debugging script.

--- debugging-script.py
+++ debugging-script.py
@@ -1,13 +1,28 @@
 # -------------------------------------------------------------------------------------
 from cassian.models import CassianModel
+from cassian.data_management import Dataset
 
-dataset = 'dataset-103/ready-dataset.pkl'
-cassian = CassianModel( dataset, batch_size = 16, timesteps = 90)
+dataset = 'dataset-103/raw-data.pkl'
+ds = Dataset(dataset)
 
-cassian.model.summary()
-cassian.plot_model()
-
-cassian.train_on_dataset()
+#dataset = 'dataset-103/ready-dataset.pkl'
+#cassian = CassianModel( dataset, batch_size = 16, timesteps = 90)
+#
+#vec_mean = cassian.dataset.vec_mean
+#powers = 1 / vec_mean
+#threshold = powers.mean() + 1.5 * powers.std()
+#powers = powers.clip( max = threshold)
+#
+#product = cassian.dataset()[0]
+#product_obj = cassian.dataset(product)
+#vec = product_obj.vec
+#
+#sample_vec = vec * powers
+#
+#cassian.model.summary()
+#cassian.plot_model()
+#
+#cassian.train_on_dataset()
 
 ## -------------------------------------------------------------------------------------
 #from cassian.connectivity import DatabaseClient
