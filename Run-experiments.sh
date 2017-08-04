6725a37a55c6b65aa4a108cac796d92deb266a7f
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-08-03 21:51:07 -0500

    Model trains nicely but is biased-down for some SKUs.

--- Run-experiments.sh
+++ Run-experiments.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 STORE=103
-EPOCHS_PER_RUN=800
+EPOCHS_PER_RUN=1000
 BATCH_SIZE=256
 
 rm trained-models/*
 rm results/*
 rm -r tensorboard-logs/*
 
-for REGULARIZATION in 1E-4 2E-4 4E-4 1E-3
+for REGULARIZATION in 4E-4 1E-3 2E-3 4E-3
 do
-for LEARNING_RATE in 1E-3 2E-3
+for LEARNING_RATE in 1E-3 1E-4
 do
 ./Cassian.py --store $STORE --train --epochs $EPOCHS_PER_RUN --batch-size $BATCH_SIZE --regularization $REGULARIZATION --learning-rate $LEARNING_RATE
 done
