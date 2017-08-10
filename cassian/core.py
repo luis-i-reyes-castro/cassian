2ea9ebdc22d103cad4c0e8e07e4551b5bbc26947
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2017-08-09 22:43:02 -0500

    Removes differentiator units, changes channel selector activation to ELU, and allows integrator to both decay and accumulate beyond (-1,+1).

--- cassian/core.py
+++ cassian/core.py
@@ -59,8 +59,6 @@ class NonlinearPID ( Layer ) :
                   mat_W_p_regularizer = None,
                   mat_W_i_initializer = 'glorot_uniform',
                   mat_W_i_regularizer = None,
-                  mat_W_d_initializer = 'glorot_uniform',
-                  mat_W_d_regularizer = None,
                   activity_regulizer = None,
                   dropout_u = 0.,
                   dropout_x = 0., **kwargs ) :
@@ -97,8 +95,6 @@ class NonlinearPID ( Layer ) :
 
         self.mat_W_i_initializer = initializers.get(mat_W_i_initializer)
         self.mat_W_i_regularizer = regularizers.get(mat_W_i_regularizer)
-        self.mat_W_d_initializer = initializers.get(mat_W_d_initializer)
-        self.mat_W_d_regularizer = regularizers.get(mat_W_d_regularizer)
 
         self.activity_regularizer = regularizers.get(activity_regulizer)
 
@@ -162,23 +158,23 @@ class NonlinearPID ( Layer ) :
         self.state_spec =   InputSpec( shape = ( batch_size, self.units) )
 
         self.mat_R_z = self.add_weight( name = 'mat_R_z',
-                                        shape = ( U_dim, 3 * self.units),
+                                        shape = ( U_dim, 2 * self.units),
                                         initializer = self.mat_R_z_initializer,
                                         regularizer = self.mat_R_z_regularizer)
 
         self.vec_b_z = self.add_weight( name = 'vec_b_z',
-                                        shape = ( 1, 3 * self.units),
+                                        shape = ( 1, 2 * self.units),
                                         initializer = self.vec_b_z_initializer,
                                         regularizer = self.vec_b_z_regularizer)
         self.vec_b_z = K.tile( self.vec_b_z, ( batch_size, 1) )
 
         self.mat_R_0 = self.add_weight( name = 'mat_R_0',
-                                        shape = ( U_dim, self.units + X_dim),
+                                        shape = ( U_dim, self.units),
                                         initializer = self.mat_R_0_initializer,
                                         regularizer = self.mat_R_0_regularizer)
 
         self.vec_b_0 = self.add_weight( name = 'vec_b_0',
-                                        shape = ( 1, self.units + X_dim),
+                                        shape = ( 1, self.units),
                                         initializer = self.vec_b_0_initializer,
                                         regularizer = self.vec_b_0_regularizer)
         self.vec_b_0 = K.tile( self.vec_b_0, ( batch_size, 1) )
@@ -198,20 +194,13 @@ class NonlinearPID ( Layer ) :
                                         initializer = self.mat_W_i_initializer,
                                         regularizer = self.mat_W_i_regularizer)
 
-        self.mat_W_d = self.add_weight( name = 'mat_W_d',
-                                        shape = ( X_dim, self.units),
-                                        initializer = self.mat_W_d_initializer,
-                                        regularizer = self.mat_W_d_regularizer)
-
         self.initial_i = K.zeros( ( batch_size, self.units) )
-        self.initial_x = K.zeros( ( batch_size, X_dim) )
 
         if self.stateful :
-            self.states = [ K.zeros_like(self.initial_i),
-                            K.zeros_like(self.initial_x) ]
+            self.states = [ K.zeros_like(self.initial_i) ]
             self.reset_states()
         else :
-            self.states = [ None, None ]
+            self.states = [ None ]
 
         super( NonlinearPID, self).build( input_shape)
 
@@ -250,18 +239,16 @@ class NonlinearPID ( Layer ) :
 
         # Computes U-input-dependent biases
         U_bias   = K.dot( U_vecs, self.mat_R_b) # shp = ( batch, 2 * units)
-        U_bias   = K.reshape( U_bias, ( -1, self.units, 2) ) # shp = ( batch, units, 2)
-        U_bias_p = U_bias[ :, :, 0] # shape = ( batch_size, units)
-        U_bias_i = U_bias[ :, :, 1] # shape = ( batch_size, units)
+        U_bias_p = U_bias[ :, : self.units ]
+        U_bias_i = U_bias[ :, self.units : ]
 
         # Computes channel selectors (i.e. Z-vectors) from U-inputs, resulting in
         # tensors Z_p, Z_i and Z_d, each with shape ( batch_size, units).
-        Z_pid = K.dot( U_vecs, self.mat_R_z) + self.vec_b_z # shp = ( batch, 3 * units)
-        Z_pid = K.reshape( Z_pid, ( -1, self.units, 3) ) # shp = ( batch, units, 3)
-        Z_pid = K.exp( Z_pid)
-        Z_p   = Z_pid[ :, :, 0]
-        Z_i   = Z_pid[ :, :, 1]
-        Z_d   = Z_pid[ :, :, 2]
+        Z_pid = K.dot( U_vecs, self.mat_R_z) + self.vec_b_z
+        # shape = ( batch_size, 2 * units)
+        Z_pid = K.elu( Z_pid, alpha = 0.1)
+        Z_p   = Z_pid[ :, : self.units ]
+        Z_i   = Z_pid[ :, self.units : ]
 
         # Builds X-inputs dropout mask if applicable
         if 0 < self.dropout_x < 1 :
@@ -270,21 +257,13 @@ class NonlinearPID ( Layer ) :
             X_mask = K.in_train_phase( X_mask, X_ones, training)
 
         # Defines list of initial states. At the end list_of_initial_states
-        # contains two tensors: the first is the initial integral term with
-        # shape ( batch_size, units); the second is the previous input with
-        # shape ( batch_size, X_dim).
+        # contains one tensor: the first is the initial integral term with
+        # shape ( batch_size, units).
         if self.stateful :
             list_of_initial_states = self.states
         else :
-            initial_ix = K.dot( U_vecs, self.mat_R_0) + self.vec_b_0
-            # shape = ( batch_size, units + X_dim)
-            self.initial_i += initial_ix[ :, : self.units ] # shp = ( batch, units)
-            self.initial_x += initial_ix[ :, self.units : ] # shp = ( batch, X_dim)
-            list_of_initial_states = [ self.initial_i, self.initial_x ]
-
-        # Applies dropout to the initial X-input if applicable
-        if 0 < self.dropout_x < 1 :
-            list_of_initial_states[1] *= X_mask
+            self.initial_i += K.dot( U_vecs, self.mat_R_0) + self.vec_b_0
+            list_of_initial_states = [ self.initial_i ]
 
         # -----------------------------------------------------------------------------
         # For each timestep t ...
@@ -294,30 +273,23 @@ class NonlinearPID ( Layer ) :
             if self.dropout_x :
                 X_t = X_t * X_mask
 
-            # List of previous states contains two tensors:
+            # List of previous states contains one tensor:
             # Previous integral I_{t-1} with shape = ( batch_size, units);
-            # Previous input X_{t-1} with shape = ( batch_size, X_dim).
             I_tm1 = list_of_previous_states[0]
-            X_tm1 = list_of_previous_states[1]
 
             # Computes (P) proportional, (I) integral and (D) difference terms.
             # Each of them has shape ( batch_size, units).
             P_t = self.activation( K.dot( X_t, self.mat_W_p) + U_bias_p )
-            I_t = self.activation( I_tm1 + K.dot( X_t, self.mat_W_i) + U_bias_i )
-            D_t = self.activation( K.dot( X_t - X_tm1, self.mat_W_d) )
+            I_t = 0.99 * I_tm1 \
+                + 0.20 * self.activation( K.dot( X_t, self.mat_W_i) + U_bias_i )
 
             # Computes output tensor, which has shape ( batch_size, units).
-            Y_t = ( Z_p * P_t ) + ( Z_i * I_t ) + ( Z_d * D_t )
+            Y_t = ( Z_p * P_t ) + ( Z_i * I_t )
 
             if 0 < self.dropout_u + self.dropout_x :
                 Y_t._uses_learning_phase = True
 
-            # List of current states contains two tensors:
-            # Current integral I_t with shape = ( batch_size, units);
-            # Current input X_t with shape = ( batch_size, X_dim).
-            list_of_states = [ I_t, X_t ]
-
-            return ( Y_t, list_of_states)
+            return ( Y_t, [ I_t ] )
 
         # -----------------------------------------------------------------------------
         last_output, outputs, states = K.rnn( step_function = recursion,
@@ -358,11 +330,10 @@ class NonlinearPID ( Layer ) :
                               str(correct_shape) + ' but got passed an array ' +
                               'of shape ' + str( U_vecs.shape ) + '.' )
 
-        initial_ix = np.dot( U_vecs, K.get_value(self.mat_R_0) ) \
-                   + K.get_value( self.vec_b_0)
+        initial_i = np.dot( U_vecs, K.get_value(self.mat_R_0) ) \
+                  + K.get_value( self.vec_b_0)
 
-        K.set_value( self.initial_i, initial_ix[ :, : self.units ] )
-        K.set_value( self.initial_x, initial_ix[ :, self.units : ] )
+        K.set_value( self.initial_i, initial_i)
 
         self.reset_states()
 
@@ -375,7 +346,6 @@ class NonlinearPID ( Layer ) :
             raise AttributeError( 'Layer must be stateful.' )
 
         K.set_value( self.states[0], K.get_value(self.initial_i) )
-        K.set_value( self.states[1], K.get_value(self.initial_x) )
 
         return
 
